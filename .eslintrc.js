ab4cc2616180f827ac3f54802c1904ef500b753f
Czy <651525974@qq.com>
2021-11-18 21:04:15 +0800

    init react typescript scss eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    overrides: [
+        {
+            files: ["*.ts"],
+            parser: "@typescript-eslint/parser",
+            plugins: ["@typescript-eslint"],
+            extends: ["plugin:@typescript-eslint/recommended"],
+        },
+        {
+            files: ["*.tsx"],
+            parser: "@typescript-eslint/parser",
+            plugins: ["react", "react-hooks", "@typescript-eslint/eslint-plugin"],
+            extends: ["plugin:@typescript-eslint/recommended"],
+        },
+    ],
+    env: {
+        browser: true,
+        node: true,
+        commonjs: true,
+        es2021: true,
+    },
+    rules: {
+        // 分号
+        "semi": "error",
+        // 对象键值引号样式保持一致
+        "quote-props": ["error", "consistent-as-needed"],
+        // 箭头函数允许单参数不带括号
+        "arrow-parens": ["error", "as-needed"],
+        // no var
+        "no-var": "error",
+        // const
+        "prefer-const": "error",
+        // 允许console
+        "no-console": "off",
+    },
+};
