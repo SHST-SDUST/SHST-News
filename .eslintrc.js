d3152b5fb18da6559ff86bd271e2e340cb87af9c
Czy <651525974@qq.com>
2021-11-19 11:15:47 +0800

    change to pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,7 @@ module.exports = {
         "prefer-const": "error",
         // 允许console
         "no-console": "off",
+        // 关闭每个函数都要显式声明返回值
+        "@typescript-eslint/explicit-module-boundary-types": "off",
     },
 };
