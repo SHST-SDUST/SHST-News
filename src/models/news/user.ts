3a59aecee945bf47a03e3a7c952b72afff31cea8
Czy <651525974@qq.com>
2021-12-06 16:12:36 +0800

    rename

---
+++ src/models/news/user.ts
@@ -0,0 +1,21 @@
+import { data } from "src/modules/global-data";
+import { request, WrapperResponse } from "src/modules/request";
+import { toast } from "src/modules/toast";
+
+export const getUserInfo = () => {
+    type Response = {
+        user: null | { nick_name: string; avatar_url: string };
+        update: boolean;
+        notice: number;
+    };
+    return new Promise<WrapperResponse<Response>>(resolve => {
+        request<Response>({
+            url: data.url + "/news/home/getUserInfo",
+        })
+            .then(res => resolve(res))
+            .catch(() => {
+                toast("获取用户信息失败，请稍后重试", "error");
+                console.log("getUserInfo", "reject");
+            });
+    });
+};
