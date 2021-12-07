2c47fda0013387462b2915d4b200f2f79bfd88af
Czy <651525974@qq.com>
2021-12-07 19:31:03 +0800

    增加路由守卫 处理首页与详情keepalive问题(破环性组件，可能造成未知问题)

--- src/views/news/index.tsx
+++ src/views/news/index.tsx
@@ -1,20 +1,42 @@
 import News from "./news";
 import Publish from "./publish";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "../system/NotFound";
 import MyNewsList from "./mine/news";
 import Detail from "./detail";
+import { data } from "src/modules/global-data";
+import { AliveScope, KeepAlive } from "react-activation";
 
-const NewsRouter = (): JSX.Element => (
-    <>
-        <Routes>
-            <Route path="/" element={<News />}></Route>
-            <Route path="/detail/:id" element={<Detail />}></Route>
-            <Route path="/publish" element={<Publish />}></Route>
-            <Route path="/mine/news" element={<MyNewsList />}></Route>
-            <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-    </>
-);
+const NewsRouter = (): JSX.Element => {
+    const getUserStatus = () => data.user; // 未使用状态管理 直接检测守卫
+    const RedirectIndex = <Navigate replace={true} to="/" />;
+    return (
+        <AliveScope>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <KeepAlive name="news-index">
+                            <News />
+                        </KeepAlive>
+                    }
+                ></Route>
+                <Route path="/detail/:id" element={<Detail />}></Route>
+                {getUserStatus() ? (
+                    <>
+                        <Route path="/publish" element={<Publish />}></Route>
+                        <Route path="/mine/news" element={<MyNewsList />}></Route>
+                    </>
+                ) : (
+                    <>
+                        <Route path="/publish" element={RedirectIndex}></Route>
+                        <Route path="/mine/news" element={RedirectIndex}></Route>
+                    </>
+                )}
+                <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+        </AliveScope>
+    );
+};
 
 export default NewsRouter;
